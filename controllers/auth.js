d79d1fe926662acb1d7a7832a3ce43d08a3a3877
Zac Smith <zrs3141592@gmail.com>
2020-05-19 16:49:42 -0600

    user add to db

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,27 @@
-exports.signup = (req, res) => {
-  console.log("REQ BODY ON SIGNUP", req.body);
+const User = require("../models/user.js");
 
-  res.json({
-    message: "Server is running at /signup",
+exports.signup = (req, res) => {
+  //   console.log("REQ BODY ON SIGNUP", req.body);
+  const { name, email, password } = req.body;
+  User.findOne({ email }).exec((err, user) => {
+    if (user) {
+      return res.status(400).json({
+        error: "Email is taken",
+      });
+    }
+  });
+
+  let newUser = new User({ name, email, password });
+
+  newUser.save((err, success) => {
+    if (err) {
+      console.log("sign up error", err);
+      return res.status(400).json({
+        error: err,
+      });
+    }
+    res.json({
+      message: "Signup success! Please signin",
+    });
   });
 };
--- models/user.js
+++ models/user.js
@@ -1,4 +1,4 @@
-const mongoose = requrie("mongoose");
+const mongoose = require("mongoose");
 const crypto = require("crypto");
 
 // user schema
@@ -49,13 +49,16 @@ userSchema
 // methods
 userSchema.methods = {
   authenticate: function (plainText) {
-    return this.encryptPassword(plainText) === this.hashed_password;
+    return this.encryptPassword(plainText) === this.hashed_password; // true false
   },
 
   encryptPassword: function (password) {
     if (!password) return "";
     try {
-      return crypto.createHmac("sha1", secret).update(password).digest("hex");
+      return crypto
+        .createHmac("sha1", this.salt)
+        .update(password)
+        .digest("hex");
     } catch (err) {
       return "";
     }
