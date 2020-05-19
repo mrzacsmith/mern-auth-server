a043967465a68abd866302ca93aff6629d15bf59
Zac Smith <zrs3141592@gmail.com>
2020-05-18 18:20:17 -0600

    create user model

---
+++ models/user.js
@@ -0,0 +1,38 @@
+const mongoose = requrie("mongoose");
+const crypto = require("crypto");
+
+// user schema
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      max: 32,
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      lowercase: true,
+    },
+    hashed_password: {
+      type: String,
+      required: true,
+    },
+    salt: String,
+    role: {
+      type: String,
+      default: "subscriber",
+    },
+    resetPasswordLink: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+// virtual
+
+// methods
