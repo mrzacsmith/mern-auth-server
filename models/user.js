6067086f81419e9602c7429ff35f78c6899e6edf
Zac Smith <zrs3141592@gmail.com>
2020-05-18 18:35:49 -0600

    create methods and clean up

--- models/user.js
+++ models/user.js
@@ -5,24 +5,25 @@ const crypto = require("crypto");
 const userSchema = new mongoose.Schema(
   {
     name: {
-      data: String,
+      type: String,
       trim: true,
       required: true,
       max: 32,
     },
     email: {
-      data: String,
+      type: String,
       trim: true,
       required: true,
+      unique: true,
       lowercase: true,
     },
     hashed_password: {
-      data: String,
+      type: String,
       required: true,
     },
     salt: String,
     role: {
-      data: String,
+      type: String,
       default: "subscriber",
     },
     resetPasswordLink: {
@@ -34,5 +35,35 @@ const userSchema = new mongoose.Schema(
 );
 
 // virtual
+userSchema
+  .virtual("password")
+  .set(function (password) {
+    this._password = password;
+    this.salt = this.makeSalt();
+    this.hashed_password = this.encryptPassword(password);
+  })
+  .get(function () {
+    return this._password;
+  });
 
 // methods
+userSchema.methods = {
+  authenticate: function (plainText) {
+    return this.encryptPassword(plainText) === this.hashed_password;
+  },
+
+  encryptPassword: function (password) {
+    if (!password) return "";
+    try {
+      return crypto.createHmac("sha256", secret).update(password).digest("hex");
+    } catch (err) {
+      return "";
+    }
+  },
+
+  makeSalt: function () {
+    return Math.round(new Date().valueOf() * Math.random()) + "";
+  },
+};
+
+module.exports = mongoose.model("User", userSchema);
