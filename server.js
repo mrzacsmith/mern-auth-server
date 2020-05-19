becb7da1bdfcacbafcfbcbfea0ad67238e1e1fd2
Zac Smith <zrs3141592@gmail.com>
2020-05-18 18:51:18 -0600

    add helmet, create .env

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const helmet = require("helmet");
+require("dotenv").config();
+
 const app = express();
 
 // import routes
 const authRoutes = require("./routes/auth.js");
+
+// app middleware
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+// app.use(cors())
+if (process.env.NODE_ENV === "development") {
+  app.use(cors({ origin: `http://localhost:3000` }));
+}
 
 // middleware
 app.use("/api", authRoutes);
@@ -13,8 +29,8 @@ app.get("/", (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 7500;
 
 app.listen(PORT, () => {
-  console.log(`\n** Server is running ${PORT} **\n`);
+  console.log(`\n** Server is running ${PORT} - ${process.env.NODE_ENV}**\n`);
 });
