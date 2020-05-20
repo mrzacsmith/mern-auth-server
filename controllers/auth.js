b4b0eab510d66c9af97bceea2f15401d89819794
Zac Smith <zrs3141592@gmail.com>
2020-05-19 23:54:10 -0600

    start sendmail signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,19 +1,36 @@
 const User = require("../models/user.js");
+const jwt = require("jsonwebtoken");
+const sgMail = require("@sendgrid/mail");
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 exports.signup = (req, res) => {
-  //
+  const { name, email, password } = req.body;
+  User.findOne({ email }).exec((err, user) => {
+    if (user) {
+      return res.status(400).json({
+        error: "Email is taken",
+      });
+    }
+  });
+
+  const token = jwt.sign(
+    { name, email, password },
+    process.env.JWT_ACCOUNT_ACTIVATION,
+    { expiresIn: "10m" }
+  );
 };
 
 // exports.signup = (req, res) => {
 //   //   console.log("REQ BODY ON SIGNUP", req.body);
 //   const { name, email, password } = req.body;
-//   User.findOne({ email }).exec((err, user) => {
-//     if (user) {
-//       return res.status(400).json({
-//         error: "Email is taken",
-//       });
-//     }
-//   });
+// User.findOne({ email }).exec((err, user) => {
+//   if (user) {
+//     return res.status(400).json({
+//       error: "Email is taken",
+//     });
+//   }
+// });
 
 //   let newUser = new User({ name, email, password });
 
